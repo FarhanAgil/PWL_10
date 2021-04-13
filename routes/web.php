bbaf18f8e9b2dd49b1d4199c568b338b6b58207e
FarhanAgil <drakek1977@gmail.com>
2021-04-13 22:23:41 +0700

    melakukan mirroring file dan mengoreksi beberapa error

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('articles', [ArticleController::class]);
+Route::resource('articles', ArticleController::class);
 Route::get('/', function () {
     return view('welcome');
 });
